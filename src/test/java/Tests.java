ba784dd6ab2f078a8fb12d38ae6c8917ffd71f73
jlopez <jlopez@timetrade.com>
2021-01-29 12:51:33 -0300

    fix imports

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -3,7 +3,6 @@ import org.openqa.selenium.NoSuchElementException;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import pages.MainPage;
 
